cd2847a947310dff01582d47912a086efd337447
samba <gudurusamba@gmail.com>
2023-05-15 12:09:19 +0530

    terraform example

---
+++ forntend.sh
@@ -0,0 +1,9 @@
+yum install nginx -y
+cp roboshop.conf /etc/nginx/defualt.d/roboshop.conf
+systemctl enable nginx
+systemctl start nginx
+rm -rf /usr/share/nginx/html/*
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+systemctl restart nginx
